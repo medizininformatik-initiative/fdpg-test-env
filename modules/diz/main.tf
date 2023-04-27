9cfbfd72e9b478df391da54041945401b7bcd64a
Alexander Kiel <alexanderkiel@gmx.net>
2023-04-27 21:07:44 +0200

    Add Port 80 to Firewall

--- modules/diz/main.tf
+++ modules/diz/main.tf
@@ -13,6 +13,16 @@ resource "hcloud_firewall" "dmz" {
   rule {
     direction  = "in"
     protocol   = "tcp"
+    port       = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
     port       = "443"
     source_ips = [
       "0.0.0.0/0",
