251e8582404efc07bc3b825095f1570c154f439b
Alexander Kiel <alexanderkiel@gmx.net>
2023-05-16 14:33:37 +0200

    Lower Perf Test Volume Size

--- modules/perf-test/main.tf
+++ modules/perf-test/main.tf
@@ -6,29 +6,29 @@ resource "hcloud_primary_ip" "perf_test" {
   auto_delete   = false
 }
 
-#resource "hcloud_server" "perf_test" {
-#  name         = "perf-test"
-#  image        = "ubuntu-22.04"
-#  server_type  = "cax41"
-#  datacenter   = "fsn1-dc14"
-#  ssh_keys     = var.ssh_keys
-#
-#  public_net {
-#    ipv4         = hcloud_primary_ip.perf_test.id
-#    ipv4_enabled = true
-#    ipv6_enabled = true
-#  }
-#}
-#
-#resource "hcloud_volume" "test_data" {
-#  name      = "test-data"
-#  size      = 1024
-#  location  = "fsn1"
-#  format    = "ext4"
-#}
-#
-#resource "hcloud_volume_attachment" "perf_test_test_data" {
-#  server_id = hcloud_server.perf_test.id
-#  volume_id = hcloud_volume.test_data.id
-#  automount = true
-#}
+resource "hcloud_server" "perf_test" {
+  name         = "perf-test"
+  image        = "ubuntu-22.04"
+  server_type  = "cax41"
+  datacenter   = "fsn1-dc14"
+  ssh_keys     = var.ssh_keys
+
+  public_net {
+    ipv4         = hcloud_primary_ip.perf_test.id
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
+
+resource "hcloud_volume" "test_data" {
+  name      = "test-data"
+  size      = 512
+  location  = "fsn1"
+  format    = "ext4"
+}
+
+resource "hcloud_volume_attachment" "perf_test_test_data" {
+  server_id = hcloud_server.perf_test.id
+  volume_id = hcloud_volume.test_data.id
+  automount = true
+}
