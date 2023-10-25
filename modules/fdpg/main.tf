8ab08313178fec34a081a21ec3a963fcd2816a73
Mathias Rühle <math2306@hotmail.com>
2023-10-25 16:33:33 +0200

    Remove Extra DSF FHIR Host
    Keep all portal services on one machine similar to current production
    setup.

--- modules/fdpg/main.tf
+++ modules/fdpg/main.tf
@@ -76,26 +76,3 @@ resource "hcloud_server" "fdpg-keycloak" {
     ipv6_enabled = true
   }
 }
-
-resource "hcloud_primary_ip" "fdpg_dsf_fhir" {
-  name          = terraform.workspace == "default" ? "fdpg-dsf-fhir" : format("fdpg-dsf-fhir-%s", terraform.workspace)
-  type          = "ipv4"
-  datacenter    = "fsn1-dc14"
-  assignee_type = "server"
-  auto_delete   = false
-}
-
-resource "hcloud_server" "fdpg-dsf-fhir" {
-  name         = terraform.workspace == "default" ? "fdpg-dsf-fhir" : format("fdpg-dsf-fhir-%s", terraform.workspace)
-  image        = "ubuntu-22.04"
-  server_type  = "cx11"
-  datacenter   = "fsn1-dc14"
-  ssh_keys     = var.ssh_keys
-  firewall_ids = [hcloud_firewall.fdpg.id]
-
-  public_net {
-    ipv4         = hcloud_primary_ip.fdpg_dsf_fhir.id
-    ipv4_enabled = true
-    ipv6_enabled = true
-  }
-}
